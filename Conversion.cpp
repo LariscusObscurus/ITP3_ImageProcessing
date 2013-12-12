2c547107b179823e8e6b151d69b5619184d93d17
Leonhardt Schwarz <if12b076@technikum--wien.at>
2013-12-12 03:42:11 +0100

    Formatbug behoben?

--- Conversion.cpp
+++ Conversion.cpp
@@ -7,12 +7,12 @@ int ConvertQformat(QImage::Format format)
 	switch (format) {
 	case QImage::Format_RGB888:
 		return CV_8UC3;
-	case QImage::Format_ARGB32:
-		return CV_8UC4;
+	/*case QImage::Format_ARGB32:
+		return CV_8UC4;*/
 	case QImage::Format_Mono:
 		return CV_8UC1;
 	default:
-		return CV_8UC3;
+		return CV_8UC4;
 	}
 }
 
@@ -21,12 +21,12 @@ QImage::Format ConvertCvFormat(int format)
 	switch (format) {
 	case CV_8UC3:
 		return QImage::Format_RGB888;
-	case CV_8UC4:
-		return QImage::Format_ARGB32;
+	/*case CV_8UC4:
+		return QImage::Format_ARGB32;*/
 	case CV_8UC1:
 		return QImage::Format_Mono;
 	default:
-		return QImage::Format_RGB888;
+		return QImage::Format_ARGB32;
 	}
 }
 
